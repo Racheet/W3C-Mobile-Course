6a9976b46db9b5346b1ea054841727a6e78e702e
Racheet Dave <racheet@gmail.com>
2012-06-16 18:10:54 +0000

    sorted out script file

--- assignment4/js/script.js
+++ assignment4/js/script.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
+    //Remove progressive enchancement fallback styles
     $("dl").hide();
     $(".center").removeClass("center");
     $("h2").each(function(){$(this).addClass("right-arrow");});
-    $("h2").click(function(){
-        $(this).parent().children('dl').toggle();
-        $('dl').click(function(){$(this).toggle();});
-    });
+    // Set up click handlers
+    (function(){
+        var toggleArrow = function(){
+            var testedObject = $(this);
+            if (testedObject.hasClass("right-arrow")){
+              testedObject.removeClass("right-arrow");
+              testedObject.addClass("down-arrow");
+            } else if (testedObject.hasClass("down-arrow")) {
+              testedObject.removeClass("down-arrow");
+              testedObject.addClass("right-arrow");  
+            }
+        };
+    
+        
+        $("h2").click(function(){
+            $(this).parent().children('dl').toggle();
+            toggleArrow();
+        });
+        $('dl').click(function(){
+            $(this).toggle();
+            toggleArrow();
+        });
+    })();
 });
