0071cfac032a410804167c974065c120d2cb00df
Racheet Dave <racheet@gmsil.com>
2012-06-17 16:46:31 +0100

    smushed script

--- assignment4/index.php
+++ assignment4/index.php
@@ -160,7 +160,7 @@ include($root . '/inc/switcher.php');
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
   <?php echoSwitch('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write(\'<script src="js/libs/jquery-1.7.1.min.js"><\/script>\')</script>
-  <script src="js/script.js"></script>', ''); ?>
+  <script src="js/script.min.js"></script>', ''); ?>
 
 
 </body>
